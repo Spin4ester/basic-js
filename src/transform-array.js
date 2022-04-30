7e4681fdf6802598552a51cfd5667ea7c0fb7267
Spin4ester <bakibayev.r@gmail.com>
2022-04-30 23:58:08 +0200

    solve transform array partially

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  /*check if its an array*/
+  if(Array.isArray(arr)) {
+    /*check if its empty*/
+    if(arr.length !== 0) {
+      /*check if its not simple arr*/
+      if(arr.includes('--discard-next') || arr.includes('--discard-prev') || arr.includes('--double-prev') || arr.includes('--double-next')) {
+       
+        let arrNew = [...arr];
+    for (let i = 0; i < arrNew.length; i++) {
+      if(arrNew[i] === '--discard-next') {
+        if(arrNew.indexOf(arrNew[i]) === arrNew.indexOf(arrNew[arrNew.length-1])) {
+          arrNew.splice([i],1)
+          return arrNew;
+        } else {
+          return arrNew.splice([i],2)
+    }
+      } else if (arrNew[i] === '--discard-prev') {
+        if(arrNew.indexOf(arrNew[i]) === 0) {
+          
+          arrNew.splice([i],1)
+          return arrNew;
+          
+        } else {
+          return arrNew.splice([i-1],2)
+        }
+     
+      } else if (arrNew[i] === '--double-next') {
+        if(arrNew.indexOf(arrNew[i]) === arrNew.indexOf(arrNew[arrNew.length-1])) {
+          arrNew.splice([i],1)
+          return arrNew;
+        } else {
+          return arrNew[i] = arrNew[i+1] 
+        }
+     
+      } else if (arrNew[i] === '--double-prev') {
+        if(arrNew.indexOf(arrNew[i]) === 0) {
+          arrNew.splice([i],1)
+          return arrNew;
+        } else {
+          return arrNew[i] = arrNew[i-1];
+       }  
+      }
+    }
+        
+} else return arr;
+} 
+else {
+ return [];
+} 
+  }
+
+else {
+ throw new Error("'arr' parameter must be an instance of the Array!")
 }
+   
+}  
+
+        
 
 module.exports = {
   transform
